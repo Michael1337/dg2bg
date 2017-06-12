c795a0a47f16a8ae8edb5796dffda72d4a6a0db6
Michael <michael.weber1337@gmail.com>
2017-06-12 03:19:05 +0200

    Code cleanup (order, style, redundancy)

--- js/scripts.js
+++ js/scripts.js
@@ -3,42 +3,42 @@
  */
 function showWinBeliver() {
     var link = "http://www.diegilde2-browsergame.de/measure.php?ms=WinBeliver";
-    $("#responselink").html('Gläubige gewinnen');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showPrey() {
     var link = "http://www.diegilde2-browsergame.de/measure.php?ms=Prey";
-    $("#responselink").html('Predigt abhalten');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showTrain() {
     var link = "http://www.diegilde2-browsergame.de/measure.php?ms=Train";
-    $("#responselink").html('Gruppenmitglied trainieren');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showChangeFaith() {
     var link = "http://www.diegilde2-browsergame.de/measure.php?ms=ChangeFaith";
-    $("#responselink").html('Glauben wechseln');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showSpy() {
     var link = "http://www.diegilde2-browsergame.de/measure.php?ms=Spy";
-    $("#responselink").html('Sich umhören');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showSchmuggel() {
     var link = "http://www.diegilde2-browsergame.de/quests.php?m=7";
-    $("#responselink").html('Warenschmuggel');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('Ändere "chance" zu 100 und füge "&iv=[x]" hinzu.');
 
@@ -46,182 +46,182 @@ function showSchmuggel() {
 
 function showOrderCollectEvidence() {
     var link = "http://www.diegilde2-browsergame.de/measure.php?ms=OrderCollectEvidence";
-    $("#responselink").html('Sich umhören');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showQuack() {
     var link = "http://www.diegilde2-browsergame.de/measure.php?ms=Quacksalver";
-    $("#responselink").html('Quacksalben');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showPickPocket() {
     var link = "http://www.diegilde2-browsergame.de/measure.php?ms=PickPocket";
-    $("#responselink").html('Taschendiebstahl');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showMsHijack() {
     var link = "http://www.diegilde2-browsergame.de/scripts/msHijack.php?m=1";
-    $("#responselink").html('Zellenplan');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showZuber() {
     var link = "http://www.diegilde2-browsergame.de/measure.php?ms=TakeABath";
-    $("#responselink").html('In den Zuber steigen');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showBrainwash() {
     var link = "http://www.diegilde2-browsergame.de/measure.php?ms=brainwash";
-    $("#responselink").html('Gehirnwäsche');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showObserve() {
     var link = "http://www.diegilde2-browsergame.de/measure.php?ms=observe";
-    $("#responselink").html('Entführungsopfer finden');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showObserve100() {
     var link = "http://www.diegilde2-browsergame.de/scripts/doObserve.php?c=100";
-    $("#responselink").html('Entführungsopfer finden');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showBlemishchair() {
     var link = "http://www.diegilde2-browsergame.de/measure.php?ms=blemishchair";
-    $("#responselink").html('Jemanden auf den Schandstuhl setzen');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showAufschlag() {
     var link = "http://www.diegilde2-browsergame.de/measure.php?ms=aufschlag";
-    $("#responselink").html('10% Aufschlag');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showGastbestehlen() {
     var link = "http://www.diegilde2-browsergame.de/measure.php?ms=gastbestehlen";
-    $("#responselink").html('Gast bestehlen');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showPlunderguild() {
     var link = "http://www.diegilde2-browsergame.de/measure.php?ms=plunderguild";
-    $("#responselink").html('Gelder der Gilde veruntreuen');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showCelebration() {
     var link = "http://www.diegilde2-browsergame.de/measure.php?ms=Celebration";
-    $("#responselink").html('Ein Fest geben');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showHavevision() {
     var link = "http://www.diegilde2-browsergame.de/measure.php?ms=havevision";
-    $("#responselink").html('Vision erhalten');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showMakeevocation() {
     var link = "http://www.diegilde2-browsergame.de/measure.php?ms=makeevocation";
-    $("#responselink").html('Ein Gebäude verfluchen');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showSendHooker() {
     var link = "http://www.diegilde2-browsergame.de/measure.php?ms=SendHooker";
-    $("#responselink").html('Dirnen entsenden');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showSendBeggar() {
     var link = "http://www.diegilde2-browsergame.de/measure.php?ms=SendBeggar";
-    $("#responselink").html('Bettler entsenden');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showForgivesins() {
     var link = "http://www.diegilde2-browsergame.de/measure.php?ms=forgivesins";
-    $("#responselink").html('Sünden vergeben');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showInspectbusiness() {
     var link = "http://www.diegilde2-browsergame.de/measure.php?ms=inspectbusiness";
-    $("#responselink").html('Einen Betrieb inspizieren');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showHidefromoffice() {
     var link = "http://www.diegilde2-browsergame.de/measure.php?ms=Hidefromoffice";
-    $("#responselink").html('Jemanden aus dem Amt werfen');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showSeverityoflaw() {
     var link = "http://www.diegilde2-browsergame.de/measure.php?ms=severityoflaw";
-    $("#responselink").html('Härte des Gesetzes');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showSetGuildtax() {
     var link = "http://www.diegilde2-browsergame.de/measure.php?ms=SetGuildtax";
-    $("#responselink").html('Gildensteuer ändern');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showNewLawKIDNAP() {
     var link = "http://www.diegilde2-browsergame.de/scripts/msSetLaw.php?law=KIDNAP";
-    $("#responselink").html('Gesetz - Entführung- ändern');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showNewLawDRINKS() {
     var link = "http://www.diegilde2-browsergame.de/scripts/msSetLaw.php?law=DRINKS";
-    $("#responselink").html('Gesetz -Tränke & Brodem- ändern.');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showNewLawFRAUD() {
     var link = "http://www.diegilde2-browsergame.de/scripts/msSetLaw.php?law=FRAUD";
-    $("#responselink").html('Gesetz - Betrügerei - ändern');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
 
 function showNewLawMAGIE() {
     var link = "http://www.diegilde2-browsergame.de/scripts/msSetLaw.php?law=MAGIE";
-    $("#responselink").html('Gesetz - Benutzen von Magie - ändern');
+    $("#responselink").html('');
     $("#response").html('<object data=' + link + ' />');
     $("#action").html('');
 }
