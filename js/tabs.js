b5a9feb5bef9f13b786c1cf0d0d6b1f01e2a3c69
Michael <michael.weber1337@gmail.com>
2017-06-12 03:43:59 +0200

    Add tabs and make app more usable

---
+++ js/tabs.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Michael W on 12.06.2017.
+ */
+$(document).ready(function(){
+
+    $('ul.tabs li').click(function(){
+        var tab_id = $(this).attr('data-tab');
+
+        $('ul.tabs li').removeClass('current');
+        $('.tab-content').removeClass('current');
+
+        $(this).addClass('current');
+        $("#"+tab_id).addClass('current');
+    })
+
+})
